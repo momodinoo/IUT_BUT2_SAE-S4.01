1219899aba04072f04a36b6a1432bd9535819715
Momodino <morgane4478@gmail.com>
2023-03-26 03:41:40 +0200

    aaaaaaaaa

--- assets/py/backend/controller/surveyController.py
+++ assets/py/backend/controller/surveyController.py
@@ -7,6 +7,21 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.route("/")
-def hello_world2():
+@app.route("/aliment_types_repartition")
+def getResults1():
+    return aliment_types_repartition()
+
+
+@app.route("/most_ate_aliment")
+def getResults2():
+    return most_ate_aliment()
+
+
+@app.route("/mean_age_by_aliment_types")
+def getResults3():
     return mean_age_by_aliment_types()
+
+
+@app.route("/mean_kcal")
+def getResults4():
+    return mean_kcal()
